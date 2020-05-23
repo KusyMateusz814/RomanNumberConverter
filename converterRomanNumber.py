54112961a1b0ed3ab5c4e50fa4fc782bfb8c17cd
Hamilkar247 <adonnibal96@gmail.com>
2020-05-24 01:26:40 +0200

    WO-5 użycie biblioteki argparse -dla parametrów- i logging -dla potrzeb debuga-

--- converterRomanNumber.py
+++ converterRomanNumber.py
@@ -1,7 +1,20 @@
 import sys
+import argparse
+import logging
 
 #roman_modern
 rom_mod = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000} 
+
+def def_params():
+    parser = argparse.ArgumentParser(
+            description='testowy opis'
+    )
+    parser.add_argument("-l", "--loghami", action='store_true',  help="increase output verbosity", required=False)
+    parser.add_argument("-c", "--convert", help="liczba do konwersji", required=True)
+    args = parser.parse_args()
+    if args.loghami:
+        logging.basicConfig(level=logging.DEBUG)
+    return args
 
 def converterModernToRoman(modernNumber):
     print("modernToRoman")
@@ -13,28 +26,33 @@ def converterModernToRoman(modernNumber):
 ### II) iteracji - sprawdzamy czy C jest mniejsze od M - jest , a wiec dodajemy mamy 1100
 ### III) iteracji - sprawdzamy czy M jest mniejsze od C - NIE jest - a więc trzeba odjąc
 ### ale nie tylko raz - a dwa, bo w poprzednim kroku zbędnie dodaliśmy
-def converterRomanToModern(romanNumber):
-    print("romanToModern")
+def converterRomanToModern(romanNumber, args):
+    if args.loghami:
+        print("romanToModern")
     val=0
     for i in range(len(romanNumber)):
-        print("i:"+str(i))
+        if args.loghami:
+            print("i:"+str(i))
         if i > 0 and rom_mod[romanNumber[i]] > rom_mod[romanNumber[i-1]]:
             val += rom_mod[romanNumber[i]] - 2 * rom_mod[romanNumber[i-1]]
         else:
             val = val + rom_mod[romanNumber[i]]
-        print(val)
+        if args.loghami:
+            print(val)
     return val
 
 def main():
-    print(sys.argv)
+    args=def_params()
+    if args.loghami:
+        logging.debug("parametry:"+str(args))
     output_number=0
     if len(sys.argv) > 1:
-        number = sys.argv[1]
+        number = args.convert
         if number.isnumeric():
-            output_number = converterModernToRoman(number)
+            output_number = converterModernToRoman(number, args)
         else:
-            output_number = converterRomanToModern(number)
-        print("Hell NO" + str(output_number))
+            output_number = converterRomanToModern(number, args)
+        print("Wynik:" + str(output_number))
 if __name__ == "__main__":
     main()
 
