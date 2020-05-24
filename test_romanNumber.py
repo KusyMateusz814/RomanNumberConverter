fd5ab3ab8b76600676e0726afd4b07b72a9388a9
Hamilkar247 <adonnibal96@gmail.com>
2020-05-24 14:35:23 +0200

    WO-6 Faza test: sprawdzenie syntax-u liczb rzymskich na wejściu

--- test_romanNumber.py
+++ test_romanNumber.py
@@ -2,6 +2,73 @@ import pytest
 from converterRomanNumber import converterRomanToModern
 from converterRomanNumber import converterModernToRoman
 
+#1. Obok siebie moga stać co najwyżej trzy znaki sposrod: I, X, C, M
+def test_syntaxInputRomanNumberXXXX():
+    result = checkSyntaxInputRomanNumber('XXXX','-l')
+ 
+    assert result
+
+def test_syntaxInputRomanNumberCCCC():
+    result = checkSyntaxInputRomanNumber('CCCC','-l')
+ 
+    assert result
+
+def test_syntaxInputRomanNumberMMMM():
+    result = checkSyntaxInputRomanNumber('MMMM','-l')
+ 
+    assert result
+
+def test_syntaxInputRomanNumberIIII():
+    result = checkSyntaxInputRomanNumber('IIII','-l')
+ 
+    assert result
+#2. Obok siebie nie mogą stać dwa znaki: V, L, D
+def test_syntaxInputRomanNumberVV():
+    result = checkSyntaxInputRomanNumber('VV','-l')
+ 
+    assert result
+
+def test_syntaxInputRomanNumberMLL():
+    result = checkSyntaxInputRomanNumber('MLL','-l')
+ 
+    assert result
+
+def test_syntaxInputRomanNumberCVV():
+    result = checkSyntaxInputRomanNumber('CVV','-l')
+ 
+    assert result
+
+#3. Nie może być dwóch znaków oznaczających liczby mniejsze bezpośrednio przed
+#znakiem oznaczającym liczbę większą
+def test_syntaxInputRomanNumberIIV():
+    result = checkSyntaxInputRomanNumber('IIV','-l')
+ 
+    assert result
+
+def test_syntaxInputRomanNumberXXC():
+    result = checkSyntaxInputRomanNumber('XXC','-l')
+ 
+    assert result
+
+def test_syntaxInputRomanNumberCCM():
+    result = checkSyntaxInputRomanNumber('LLM','-l')
+
+    assert result
+#4. Znakami poprzedzającymi znak oznaczającymi większą liczbę mogą być tylko znaki: I, X, C
+def test_syntaxInputRomanNumberII():
+    result = checkSyntaxInputRomanNumber('VX','-l')
+ 
+    assert result
+
+def test_syntaxInputRomanNumberXXC():
+    result = checkSyntaxInputRomanNumber('DM','-l')
+ 
+    assert result
+
+def test_syntaxInputRomanNumberCCM():
+    result = checkSyntaxInputRomanNumber('LM','-l')
+
+
 def test_converterRomanToModern12():
     result = converterRomanToModern('XII',True) #to samo gdyby bylo ('XII','-l')
 
